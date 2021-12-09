931ef8e0bb044b80efb342b17e714711b3ec7f59
Sarveshwar Shukla <sarveshwarshukla27@gmail.com>
2021-12-09 12:34:20 +0530

    Added first file

---
+++ HackerRank/C language/helloWorld.c
@@ -0,0 +1,13 @@
+// Printing hello world in C
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+	
+    char s[100];
+    gets(s);
+   printf("Hello, World!\n");
+   puts(s);
+    return 0;
+}
